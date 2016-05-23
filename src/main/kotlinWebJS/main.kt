b9e04ecafab22cc6389214eea0020b15a693019b
mk <turco807@gmail.com>
2016-05-22 21:49:23 -0300

    +initial example + gitignore

---
+++ src/main/kotlinWebJS/main.kt
@@ -0,0 +1,10 @@
+
+package kotlinWebJS
+import kotlin.browser.document
+
+fun main(args: Array<String>) {
+    val el = document.createElement("div")
+    el.appendChild(document.createTextNode("Hello!"))
+    document.body!!.appendChild(el)
+    println("hola!")
+}
