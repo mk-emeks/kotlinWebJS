dd0855a76b5c817e5eb7b43bff97c1a254962671
mk <turco807@gmail.com>
2016-05-22 23:16:54 -0300

    using jquery, to do: fix getJSON

--- src/main/kotlinWebJS/main.kt
+++ src/main/kotlinWebJS/main.kt
@@ -1,14 +1,32 @@
 
 package kotlinWebJS
+
 import kotlin.browser.document
+
+// using JQUERY on kotlin, memo: import jquery library!
+@native("$")
+val jquery : dynamic = noImpl
 
 fun main(args: Array<String>) {
 
     // creating a Kotlin application that compiles down to JavaScript
 
     val el = document.createElement("div")
-    el.appendChild(document.createTextNode("Hello!"))
+    el.appendChild(document.createTextNode("example"))
     document.body!!.appendChild(el)
+
+    val playersFromApi = "http://localhost:8080/"
+    // to do: fix the getJSON call -> No 'Access-Control-Allow-Origin' header is present on the requested resource. Origin 'null' is therefore not allowed access
+    jquery.getJSON(playersFromApi) { players ->
+        val playersTable = jquery("#playersTable")
+        players.forEach { player ->
+            playersTable.append("""
+                    <tr>
+                        <td><a href=${player.username}>${player.nickname}</a></td>
+                    </tr>""")
+        }
+    }
+
 
     // ---------------------------------------------------------------
     println("kotlin from js:")
